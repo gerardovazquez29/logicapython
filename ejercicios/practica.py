5c10bc88f17bb80dfbbf864c92759494c9787e59
Gerardo Santiago Vazquez Tovar <neowhatup@gmail.com>
2025-09-11 18:13:15 -0600

    jueves11

--- ejercicios/practica.py
+++ ejercicios/practica.py
@@ -9,3 +9,15 @@ print(x,y)
 nums = [1, 2, 3, 4]
 nums.append(nums.pop(1)) # pop elimina 
 print(nums) # append agrega al final
+
+# Ejercicio
+print("-"*20)
+
+students = {
+    "Ana": [8,7,9],
+    "Luis": [6,5,7],
+    "Sofia": [10,9,10]
+}
+# Agregar nuevos estudiantes
+# Scar el promedio de un estudiante existente
+# El promedio del studiante nuevo
